78fd1ac88ec501c7857c6d860ddc4bfad49daccf
akhileswar108 <vakhileswar27@gmail.com>
2023-04-08 00:11:21 +0530

    Intial Commit

---
+++ Addition.c
@@ -0,0 +1,5 @@
+int addition()
+{
+	int a=50,b=60;
+	return a+b;
+}
---
+++ Basic.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Basic.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int result=addition();
+	printf("%d",result);
+	return EXIT_SUCCESS;
+}
